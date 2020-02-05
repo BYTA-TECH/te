a1ad8b58c93f3e2b5e4ed6413d46bcfbb47f7f57
rafeekbytatech <abdul.rafeek@lxisoft.com>
2020-02-05 16:36:48 +0530

    refactor

--- src/main/java/com/bytatech/ayoos/patient/client/dms/api/RatingsApiClient.java
+++ src/main/java/com/bytatech/ayoos/patient/client/dms/api/RatingsApiClient.java
@@ -3,6 +3,6 @@ package com.bytatech.ayoos.patient.client.dms.api;
 import org.springframework.cloud.openfeign.FeignClient;
 import com.bytatech.ayoos.patient.client.dms.ClientConfiguration;
 
-@FeignClient(name="${dms.name:dms}", url="${dms.url }", configuration = ClientConfiguration.class)
+@FeignClient(name="${dms.name:dms}", url="${dms.url}", configuration = ClientConfiguration.class)
 public interface RatingsApiClient extends RatingsApi {
 }
